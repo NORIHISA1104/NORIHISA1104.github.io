0d8fa6750a3b8d6adb60aac8c114dfd0a1218aa7
NORIHISA1104 <66410485+NORIHISA1104@users.noreply.github.com>
2021-02-05 10:44:14 +0900

    Add files via upload

--- js/dateSelect.js
+++ js/dateSelect.js
@@ -1,7 +1,37 @@
 function table_scroll(){
-	var L = document.getElementById('LAYER');
-	var T = L.getElementsByTagName('table')[0];
-	L.scrollLeft = T.offsetLeft + T.rows[30].cells[0].offsetLeft;
+/*
+document.getElementById("target").scrollIntoView(true)
+
+*/
+
+
+    let tgt;
+    if ('scrollingElement' in document) {
+        tgt = document.scrollingElement;
+    } else if (this.browser.isWebKit) {
+        tgt = document.body;
+    } else {
+        tgt = document.documentElement;
+    }
+    tgt.scrollTop = 300;
+
+
+/*
+    var element = document.getElementById('target');
+    var rect = element.getBoundingClientRect();
+    var elemtop = rect.top + window.pageYOffset;
+//    alert(elemtop);
+    document.documentElement.scrollTop = elemtop;
+*/
+
+console.log(document.documentElement.scrollTop);
+
+
+//	scrollTo(100, 200);
+
+//	var L = document.getElementById('LAYER');
+//	var T = L.getElementsByTagName('table')[0];
+//	L.scrollTop = T.offsetTop + T.rows[20].cells[0].offsetTop;
 }
 
 function goDateSelect(){
