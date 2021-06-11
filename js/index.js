2ac01de0e795f67b9a9c34b5db4a012596fd4535
NORIHISA1104 <66410485+NORIHISA1104@users.noreply.github.com>
2021-06-11 14:10:34 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function goReserve(){
 	location.href = "reserve.html";
 }
+
+function changeContents(readItem, editItem, editDate){
+	document.getElementById(readItem).style.display = "none";
+	document.getElementById(editItem).style.display = "table";
+	document.getElementById(editDate).innerText = "";
+}
