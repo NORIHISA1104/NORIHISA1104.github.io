4860a8958f9570715f9d972d0a0268f9c098c3ee
NORIHISA1104 <66410485+NORIHISA1104@users.noreply.github.com>
2021-06-15 14:45:41 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ function goReserve(){
 	location.href = "reserve.html";
 }
 
-function changeContents(readItem, editItem, editDate){
-	document.getElementById(readItem).style.display = "none";
-	document.getElementById(editItem).style.display = "table";
-	document.getElementById(editDate).innerText = "";
+function changeContents(num){
+	document.getElementById('readItem' + num).style.display = "none";
+	document.getElementById('editItem' + num).style.display = "table";
+	document.getElementById('editDate' + num).innerText = "";
+	document.getElementById('editItemField' + num).focus();
 }
 
 /********************************
